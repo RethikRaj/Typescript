aac6e27deeab13d8897f13cfb0d48bf7575fada9
Rethik Raj <rethikrajrr@gmail.com>
2026-02-14 12:51:00 +0530

    Add infinite loop function example using never type

--- Basics/src/02_2_never.ts
+++ Basics/src/02_2_never.ts
@@ -7,3 +7,11 @@ function neverReturns(): never {
 // Example 2 : Never in unions
 // A never is something which is automatically removed from a type union.
 type NeverIsRemoved = string | never | number;
+
+// // Example 3 : Infinite loop function (like http servers)
+function listenAndServe(): never {
+  while (true) {
+    // Handling incoming requests...
+    console.log("Server is running...");
+  }
+}
